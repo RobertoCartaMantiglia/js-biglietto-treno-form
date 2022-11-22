3ae6839fd97f1c857f61e6413470e2e0f72356b1
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-22 15:57:28 +0100

    click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// chiedere al cliente la sua età e quanti km intende percorrere attraverso un form
+// chiedere al cliente la sua età e quanti km intende percorrere attraverso un form 
+// il prezzo del biglietto è definito in base ai km (0.2762 € al km)
+// va applicato uno sconto del 17.5% per i minorenni
+// va applicato uno sconto del 33.3% per gli over 65.
+
+
+
+
+const distance = document.getElementById("distance");
+const age = document.getElementById("age");
+const button = document.querySelector("button .btn btn-primary");
+
+
+button.addEventListener("click", function (){
+    console.log(distance.value);
+    console.log(age.value);
+})
