d676c4166a29a9823f7b0cf60ac53d1ea7465da3
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-22 17:33:49 +0100

    last mod.

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ const button = document.getElementById ("bottone");
 
 
 button.addEventListener("click", function (){
-    alert ("hai cliccato");
     console.log(distance.value);
     console.log(age.value);
 
